9c11dfd3c0319485ea03da6250ca07a5167f406b
YugNanda <yugnanda12@gmail.com>
2025-05-06 20:19:18 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+# Python Guide + 99 Projects: https://resources.legitpython.com/python-ebook-download
+
+import time
+import string
+
+letters = list(string.ascii_lowercase + string.ascii_uppercase + ' !')
+
+target = "Hello World!"
+
+current_str = ""
+
+while current_str != target:
+    for letter in letters:
+        print(current_str + letter)
+        time.sleep(0.01)
+
+        if current_str + letter == target[:len(current_str) + 1]:
+            current_str += letter
+            break
+
